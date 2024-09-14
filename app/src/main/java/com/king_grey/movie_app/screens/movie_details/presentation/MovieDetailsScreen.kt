3f7a53b7d379891da12258fa8451e2fb2721a2a4
King Grey <lohitsaidev@gmail.com>
2024-09-14 14:23:44 +0530

    Added Linear Gradient for the MovieDetailsScreen.kt

--- app/src/main/java/com/king_grey/movie_app/screens/movie_details/presentation/MovieDetailsScreen.kt
+++ app/src/main/java/com/king_grey/movie_app/screens/movie_details/presentation/MovieDetailsScreen.kt
@@ -3,21 +3,23 @@ package com.king_grey.movie_app.screens.movie_details.presentation
 import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
 import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Surface
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.compose.SubcomposeAsyncImage
 import coil.request.ImageRequest
@@ -29,30 +31,54 @@ import com.king_grey.movie_app.screens.discover.domain.model.movie.Movie
 fun SharedTransitionScope.MovieDetailsScreen(
     navController: NavHostController, movie: Movie, animatedVisibilityScope: AnimatedVisibilityScope
 ) {
-    Surface(modifier = Modifier.fillMaxSize()) {
-        val screenHeight = LocalConfiguration.current.screenHeightDp.dp
-        val imageHeight = screenHeight * 0.63f
+    val gradientBrush = Brush.verticalGradient(
+        colors = listOf(
+            Color.Transparent,
+            MaterialTheme.colorScheme.background.copy(0.2f),
+            MaterialTheme.colorScheme.background.copy(0.4f),
+            MaterialTheme.colorScheme.background.copy(0.6f),
+            MaterialTheme.colorScheme.background.copy(0.8f),
+            MaterialTheme.colorScheme.background,
+        ), startY = 1570f, endY = 1700f
+    )
+    val scrollState = rememberScrollState()
 
-        Column(modifier = Modifier.fillMaxHeight()) {
-            SubcomposeAsyncImage(modifier = Modifier
-                .fillMaxWidth()
-                .height(imageHeight)
-                .sharedElement(
-                    rememberSharedContentState(key = movie.id),
-                    animatedVisibilityScope = animatedVisibilityScope
-                ),
-                contentScale = ContentScale.FillBounds,
-                model = ImageRequest.Builder(LocalContext.current)
-                    .data("${TMDbService.IMAGE_BASE_URL}${movie.poster_path}")
-                    .crossfade(enable = true).build(),
-                contentDescription = movie.title,
-                loading = {
-                    Box(contentAlignment = Alignment.Center) {
-                        CircularProgressIndicator(
-                            modifier = Modifier.align(Alignment.Center)
-                        )
-                    }
-                })
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.fillMaxSize()) {  //.verticalScroll(scrollState)
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight(.57f)
+                    .fillMaxWidth()
+            ) {
+                SubcomposeAsyncImage(modifier = Modifier
+                    .fillMaxSize()
+                    .sharedElement(
+                        rememberSharedContentState(key = movie.id),
+                        animatedVisibilityScope = animatedVisibilityScope
+                    ),
+                    contentScale = ContentScale.FillBounds,
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data("${TMDbService.IMAGE_BASE_URL}${movie.poster_path}")
+                        .crossfade(enable = true).build(),
+                    contentDescription = movie.title,
+                    loading = {
+                        Box(contentAlignment = Alignment.Center) {
+                            CircularProgressIndicator(
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    })
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(gradientBrush)
+                )
+            }
+
+            repeat(100) {
+                Text("Hello World")
+            }
         }
     }
 }
